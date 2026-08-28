a10c63bf7c84a3cd7195abfc1ddf6be00d6ec36b
nelanjana11 <dasnelanjana@gmail.com>
2026-08-28 08:25:53 +0530

    Create sumofdigits.cpp

---
+++ sumofdigits.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, rem, sum = 0;
+
+    cout << "Enter an integer: ";
+    cin >> n;
+
+    while (n != 0)
+    {
+        rem = n % 10;
+        sum = sum + rem;
+        n = n / 10;
+    }
+
+    cout << "Sum of digits = " << sum;
+
+    return 0;
+}
