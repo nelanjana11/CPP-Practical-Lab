7eba1621ef04ea29aeb27aaa78f8553f4d4bf80e
nelanjana11 <dasnelanjana@gmail.com>
2026-08-28 08:29:38 +0530

    Create functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// Call by Value
+void callByValue(int x)
+{
+    x = x + 10;
+    cout << "Inside Call by Value: " << x << endl;
+}
+
+// Call by Reference
+void callByReference(int &x)
+{
+    x = x + 10;
+    cout << "Inside Call by Reference: " << x << endl;
+}
+
+int main()
+{
+    int a = 10, b = 10;
+
+    cout << "Before Call by Value: " << a << endl;
+    callByValue(a);
+    cout << "After Call by Value: " << a << endl;
+
+    cout << endl;
+
+    cout << "Before Call by Reference: " << b << endl;
+    callByReference(b);
+    cout << "After Call by Reference: " << b << endl;
+
+    return 0;
+}
