7b1dbdbec2900cb654bb0bb2d6f2a890bf35ffaf
nelanjana11 <dasnelanjana@gmail.com>
2026-08-28 08:11:14 +0530

    Create largest.cpp

---
+++ largest.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
+
+    if (a >= b && a >= c)
+        cout << "The largest number is " << a;
+    else if (b >= a && b >= c)
+        cout << "The largest number is " << b;
+    else
+        cout << "The largest number is " << c;
+
+    return 0;
+}
